59869a2f1d629ea3c77966078ab64e3c64d76062
msteinore <michaelsteinore@msn.com>
2019-10-28 15:27:59 -0700

    first version

---
+++ Fraction.cs
@@ -0,0 +1,168 @@
+﻿using System;
+
+namespace FractionCalc
+{
+    /// <summary>
+    /// Define a fraction class and operations required to support add, subtract, multiply, divide ops
+    /// </summary>
+    public class Fraction
+    {
+        long wholePart;  // For a mixed fraction or whole numeratorber
+        long numerator;        
+        long denominator;
+
+        public Fraction (long whole, long n, long d)
+        {
+            wholePart = whole;
+            numerator = n;
+            denominator = d;
+        }
+        
+        /// <summary>
+        /// Return simplified version of a mixed/proper/improper fraction, if possible; or original, if not.
+        /// Mixed fraction could contain improper fraction.
+        /// </summary>
+        public void simplify()
+        {
+            long gcd = GCD(abs(numerator), abs(denominator));
+            if (gcd > 1)
+            {
+                numerator = numerator / gcd;
+                denominator = denominator / gcd;
+                if (denominator == 1)
+                {
+                    if (wholePart >= 0)
+                    {
+                        wholePart += numerator;
+                    }
+                    else
+                    {
+                        wholePart -= numerator;
+                    }
+
+                    numerator = 0;
+                }
+            }
+
+            // handle improper case, which assumes fraction is not mixed.
+            if (abs(numerator) > denominator)
+            {
+                long whole = numerator / denominator;
+                numerator = abs(numerator % denominator);
+                wholePart = whole;
+            }
+        }
+
+        /// <summary>
+        /// Convert mixed fraction to proper/improper fraction
+        /// </summary>
+        public void convert()
+        {
+            if (wholePart > 0)
+            {
+                numerator += denominator * wholePart;
+            } else if (wholePart < 0) {
+                numerator = denominator * wholePart - numerator;
+            }
+
+            wholePart = 0;
+        }
+
+        /// <summary>
+        /// Multiple two non-mixed fractions
+        /// </summary>
+        public void multiply(Fraction f)
+        {
+            numerator *= f.numerator;
+            denominator *= f.denominator;
+        }
+
+        /// <summary>
+        /// Divide two non-mixed fractions
+        /// </summary>
+        public void divide(Fraction divisorF)
+        {
+            numerator *= divisorF.denominator;
+            denominator *= divisorF.numerator;
+        }
+
+        /// <summary>
+        /// Add two non-mixed fractions
+        /// </summary>
+        public void add(Fraction addendF)
+        {
+            long commonDenominator = denominator * addendF.denominator;
+            numerator = (commonDenominator / denominator * numerator) + (commonDenominator / addendF.denominator * addendF.numerator);
+            denominator = commonDenominator;
+        }
+
+        /// <summary>
+        /// Subtract one non-mixed fraction from the other
+        /// </summary>
+        /// <param name="subtrahendF"></param>
+        public void subtract(Fraction subtrahendF)
+        {
+            if (subtrahendF.wholePart != 0)
+            {
+                subtrahendF.wholePart *= -1;
+            }
+
+            else
+            {
+                subtrahendF.numerator *= -1;
+            }
+
+            add(subtrahendF);
+        }
+
+        /// <summary>
+        /// Output fraction handling various special cases
+        /// </summary>
+        public void writeResult()
+        {
+            simplify();
+            Console.Write("= ");
+            if (numerator == 0)
+            {
+                Console.WriteLine(wholePart.ToString());
+            }
+            else if (wholePart == 0 && numerator != 0)
+            {
+                Console.WriteLine(numerator.ToString() + "/" + denominator.ToString());
+            }
+            else if (wholePart != 0 && numerator != 0)
+            {
+                Console.WriteLine(wholePart.ToString() + "_" + numerator.ToString() + "/" + denominator.ToString());
+            }
+        }
+
+        /// <summary>
+        /// Return absolute value
+        /// </summary>
+        private long abs(long a)
+        {
+            return a >= 0 ? a : -a;
+        }
+
+        /// <summary>
+        /// return greatest common divisor of a and b
+        /// </summary>
+        private long GCD(long a, long b)
+        {
+            if (a == 0 || b == 0)
+            {
+                return 1;
+            }
+
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
+
+            return a == 0 ? b : a;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FractionCalc
+{
+    /// <summary>
+    /// Implements a simple fraction calculator.  
+    /// 
+    /// Design assumptions:
+    /// 1) all improper fractions will output as a mixed fraction
+    /// 2) For simplicity, apply operators in order rather than according to usual rules of operator precedence
+    /// 3) Numbers will be small enough to be stored in long type
+    /// 4) Mixed fraction input could contain improper fraction
+    /// 5) Some error checking included, but not all possible checking due to time constraint
+    /// 
+    /// </summary>
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // match 0+ whitespace, then mixed fraction or [im]proper fraction or whole number followed by space
+            string fractionPattern = @"\s*((?<whole>\d+)_(?<num>\d+)/(?<denom>\d+)|(?<num>\d+)/(?<denom>\d+)|(?<whole>\d+\s*))";                         
+
+            // match 0+ whitespace then any of 4 allowed operators
+            string operatorPattern = @"\s*(?<oper>[\+\*-/])";
+
+            Regex queryPattern = new Regex(@"
+                \s*                   # match zero or more whitespace characters, then ...
+                [\?]                  # match query operator, then ...
+                (?<remainder>.*)      # rest of input
+                ", RegexOptions.IgnorePatternWhitespace);
+
+            long whole, num, denom;
+            Fraction first;
+
+            Console.WriteLine("Enter a math expression using whole numbers, fractions and operators.  Begin with '?' symbol.");
+            Console.WriteLine("Valid operators are +, -, *, /.");
+            Console.WriteLine("Valid operands are whole numbers (x), mixed fractions (x_x/x), and proper/improper fractions (x/x).");
+            Console.WriteLine("If '?' symbol is omitted, or syntax error occurs, program ends.");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                // verify query symbol included
+                MatchCollection mc = queryPattern.Matches(input);
+                if (mc.Count == 0)
+                {
+                    Console.WriteLine("Query symbol '?' missing.");
+                    break;
+                }
+
+                // get first whole number or fraction
+                input = mc[0].Groups["remainder"].Value;
+                string firstFractionPattern = fractionPattern + @"(?<remainder>.*)";
+                Regex firstRegex = new Regex(firstFractionPattern);
+                mc = firstRegex.Matches(input);
+                if (mc.Count == 0)
+                {
+                    Console.WriteLine("First operand incorrectly specified.");
+                    break;
+                }
+
+                long.TryParse(mc[0].Groups["whole"].Value, out whole);
+                long.TryParse(mc[0].Groups["num"].Value, out num);
+                long.TryParse(mc[0].Groups["denom"].Value, out denom);
+                first = new Fraction(whole, num, denom == 0 ? 1 : denom);
+                input = mc[0].Groups["remainder"].Value;
+
+                // Get rest of operators and whole number/fractions
+                string opFractionPattern = operatorPattern + fractionPattern;
+                Regex restRegex = new Regex(opFractionPattern);
+                mc = restRegex.Matches(input);
+                if (mc.Count == 0)
+                {
+                    Console.WriteLine("Invalid expression found.");
+                    break;
+                }
+
+                //Calculate output result
+                first.convert();
+                foreach (Match m in mc)
+                {
+                    string oper = m.Groups["oper"].Value;
+                    long.TryParse(m.Groups["whole"].Value, out whole);
+                    long.TryParse(m.Groups["num"].Value, out num);
+                    long.TryParse(m.Groups["denom"].Value, out denom);
+                    Fraction next = new Fraction(whole, num, denom == 0 ? 1 : denom);
+                    next.convert();
+                    switch (oper)
+                    {
+                        case "+":
+                            first.add(next);
+                            break;
+                        case "-":
+                            first.subtract(next);
+                            break;
+                        case "*":
+                            first.multiply(next);
+                            break;
+                        case "/":
+                            first.divide(next);
+                            break;
+                    }
+                }
+
+                first.writeResult();
+            }
+        }
+    }
+}
